19421548200682b53a3862d8493ad9ba831cccf4
Iamneo <techadmin@iamneo.ai>
2023-09-01 11:42:26 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -6,9 +6,9 @@ return(
     <div key = {question.id}>
         <h4>{question.Question}  </h4>
         <Button onClickHandler={onClickHandler} children={question.Answers[0]} />
-        <Button onClickHandler={onClickHandler} children={question.Answers[0]} />
-        <Button onClickHandler={onClickHandler} children={question.Answers[0]} />
-        <Button onClickHandler={onClickHandler} children={question.Answers[0]} />
+        <Button onClickHandler={onClickHandler} children={question.Answers[1]} />
+        <Button onClickHandler={onClickHandler} children={question.Answers[2]} />
+        <Button onClickHandler={onClickHandler} children={question.Answers[3]} />
     </div>
 )
 }
