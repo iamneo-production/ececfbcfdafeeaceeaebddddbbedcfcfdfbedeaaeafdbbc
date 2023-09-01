6e0bf4293e8febdca438fc2227b8a278d46f6356
Iamneo <techadmin@iamneo.ai>
2023-09-01 11:37:22 +0000

    Backup Commit

--- reactapp/src/components/UI/Button/Button.js
+++ reactapp/src/components/UI/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>;
+const Button = ({ onClickHandler, children }) => {
+  return <button onClick={onClickHandler}>{children}</button>;
 };
 
 export default Button;
--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const Card = ({question}) => {
 return(
-    <div>
+    <div key = {question.id}>
         <h4>{question.Question}  </h4>
+        <Button onclickHandler={}>{question.Answers[0]} </Button>
+        <Button>{question.Answers[1]} </Button>
+        <Button>{question.Answers[2]} </Button>
+        <Button>{question.Answers[3]} </Button>
     </div>
 )
 }
