2684e052994b8d630d4dc7d464280998eea199cd
Iamneo <techadmin@iamneo.ai>
2023-09-05 10:08:52 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -1,23 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Button from "../Button/Button";
-import { useEffect } from "react/cjs/react.production.min";
+
 
 const Card = ({question}) => {
 
     const onClickHandler =()=>{
 console.log("first")
     }
+
     useEffect(()=>{
         console.log(question);
     })
+   
 return(
     <div key = {question.id} className="card">
         <h4>{question.Question}  </h4>
-        <button >click </button>
-        {/* <Button onClickHandler={onClickHandler} children={question.Answers[0]} />
-        <Button onClickHandler={onClickHandler} children={question.Answers[1]} />
-        <Button onClickHandler={onClickHandler} children={question.Answers[2]} />
-        <Button onClickHandler={onClickHandler} children={question.Answers[3]} /> */}
+        <Button />
+        <Button />
+        <Button />
+        <Button />
     </div>
 )
 }
