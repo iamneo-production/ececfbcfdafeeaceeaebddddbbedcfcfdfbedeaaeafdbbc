1cf6ff89d9ef4adbe3ad6d3eb1acdeb947db1740
Iamneo <techadmin@iamneo.ai>
2023-09-05 09:58:52 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -4,15 +4,16 @@ import Button from "../Button/Button";
 const Card = ({question}) => {
 
     const onClickHandler =()=>{
-
+console.log("first")
     }
 return(
     <div key = {question.id} className="card">
         <h4>{question.Question}  </h4>
-        <Button onClickHandler={onClickHandler} children={question.Answers[0]} />
+        <button >{question.Answers[0]} </button>
+        {/* <Button onClickHandler={onClickHandler} children={question.Answers[0]} />
         <Button onClickHandler={onClickHandler} children={question.Answers[1]} />
         <Button onClickHandler={onClickHandler} children={question.Answers[2]} />
-        <Button onClickHandler={onClickHandler} children={question.Answers[3]} />
+        <Button onClickHandler={onClickHandler} children={question.Answers[3]} /> */}
     </div>
 )
 }
